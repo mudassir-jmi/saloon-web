0dc47c5112220e3fff010c99a1d1102bde91a3c3
Md Mudassir <mudassirakhter420@gmail.com>
2023-04-03 13:35:08 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+let closeBtn = document.querySelector('#close-navbar');
+
+menuBtn.onclick = () =>{
+   navbar.classList.add('active');
+};
+
+closeBtn.onclick = () =>{
+    navbar.classList.remove('active');
+ };
+
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+};
